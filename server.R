efee224f1aa7050d0a9a1c27d0f57262642ae989
xl16 on lenovo <cindyxiaoxiaoli@gmail.com>
2014-06-20 19:52:04 -0600

    ignore files in .rproj.user

--- server.R
+++ server.R
@@ -8,22 +8,33 @@
 library(shiny)
 
 shinyServer(function(input, output) {
-  correctNumber <- sample(0:100,1)
+  correctNumber <- sample(1:20,1)
   attempts <<- -1
+  output$playerName <- renderText({input$playerName})
   output$userInput <- renderPrint({input$userInputID})
-  output$serverMsg <- renderText({
-    if(input$userInputID>correctNumber){
-      return("Too large!")
-    }else if (input$userInputID<correctNumber){
-      return("Too small!")
-    }else{
-      correctNumber <<- sample(0:100,1)
-      msg <- paste("Correct! Congrats! You got it right in",attempts,"attemps. New number generated, guess again!", sep=" ")
-      attempts <<- -1
-      return(msg)
-    }
-  })
   output$attempts <- renderPrint({
     attempts <<- attempts+1
     attempts+input$userInputID-input$userInputID})
+  output$serverMsg <- renderText({
+    if(input$userInputID==correctNumber){
+      msg <- paste("Correct! Congrats! You got it right in",attempts,"attemps. New number generated, guess again!", sep=" ")
+      correctNumber <<- sample(1:20,1)
+      attempts <<- 0
+      return(msg)
+    }else{
+      if(attempts>5){
+        msg<-paste("Sorry used up all 6 of your chances! The correct answer is",correctNumber,". New number generated, guess again!")
+        attempts<<- 0
+        attempts+input$userInputID-input$userInputID
+        correctNumber <<- sample(1:20,1)
+        return(msg)
+      }
+      if(input$userInputID>correctNumber){
+        return("Too high!")
+      }else if (input$userInputID<correctNumber){
+        return("Too low!")
+      }
+    }
+  })
+  
 })
--- ui.R
+++ ui.R
@@ -14,12 +14,18 @@ shinyUI(pageWithSidebar(
   
   # Sidebar with a slider input for number of observations
   sidebarPanel(
-    h4("Rules: Guess a number between 1 and 100. We will let you know if your number is coorect, or if it is too large or too small."),
-    numericInput("userInputID",'Enter your guess here',0,min=0,max=100,step=1)
+    p("I am thinking a number between 1 and 20, you have 6 tries to guess the number! I'll let you know if your guess is too high, too low, or correct!"),
+    br(),
+    p("Once you got it right I'll think of a new number between 1 and 20 and we can play again."),
+    br(),
+    textInput(inputId="playerName",label="Your Name",value="Player"),
+    numericInput("userInputID",'Enter your guess here',0,min=1,max=20,step=1)
   ),
   
   # Show a plot of the generated distribution
   mainPanel(
+    h4('Hi,'),
+    h4(textOutput("playerName")),
     h4('Attempts made:'),
     verbatimTextOutput("attempts"),
     h4('You entered:'),
