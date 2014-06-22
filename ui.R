5a788f8fb54f9b5cb4917a5d8ded4a3900674e20
xl16 on lenovo <cindyxiaoxiaoli@gmail.com>
2014-06-22 15:53:53 -0600

    added more explainations

--- ui.R
+++ ui.R
@@ -15,10 +15,10 @@ shinyUI(pageWithSidebar(
   # Sidebar with a slider input for number of observations
   sidebarPanel(
     p("I am thinking a number between 1 and 20, you have 6 tries to guess the number! I'll let you know if your guess is too high, too low, or correct!"),
+    p("Once you got it right within the 6 attempts you win, and a new game starts. If after six attempts you still haven't guessed the number you lose, I will let you know what the number is, and a new game starts."),
+    p("On the right, I will greet you if you enter your name down below. I will show you how many attempts you made in the current game, the last guess you entered, and a message after you made a guess."),
     br(),
-    p("Once you got it right I'll think of a new number between 1 and 20 and we can play again."),
-    br(),
-    textInput(inputId="playerName",label="Your Name",value="Player"),
+    textInput(inputId="playerName",label="Enter Your Name:",value="Player"),
     numericInput("userInputID",'Enter your guess here',0,min=1,max=20,step=1)
   ),
   
